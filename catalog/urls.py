ce9f47463956cd23c683ce04f984d4a018dd7dac
Александр Хасматулин <alexandr.khasmatulin@yandex.ru>
2023-06-22 21:02:11 +0300

    final

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from catalog.views import home, contacts
 
 app_name = 'catalog'
@@ -7,4 +8,4 @@ app_name = 'catalog'
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
