f165eb9daf96beaeb3460294eb8496a33d2ca002
Александр Хасматулин <alexandr.khasmatulin@yandex.ru>
2023-06-30 02:05:26 +0300

    final

--- catalog/models.py
+++ catalog/models.py
@@ -5,7 +5,7 @@ NULLABLE = {'blank':True, 'null':True}
 class Product(models.Model):
     id = models.AutoField(primary_key=True, verbose_name='id')
     name = models.CharField(max_length=150, verbose_name='Наименование')
-    description = models.TextField(max_length=500, verbose_name='Описание')
+    description = models.TextField(max_length=1000, verbose_name='Описание')
     image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100,
                               verbose_name='Изображение', **NULLABLE)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
@@ -14,7 +14,11 @@ class Product(models.Model):
     date_change = models.CharField(max_length=150, verbose_name='Дата изменения')
 
     def __str__(self):
-        return f'{self.name} {self.description}'
+        return f'''
+        {self.name}, {self.description},
+        Категория: {self.category}
+        Цена: {self.price}
+        '''
 
     class Meta:
         verbose_name = 'Продукт'
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 # Create your views here.
 def contacts(request):
     return render(request, 'catalog/contacts.html')
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list
+    }
+    return render(request, 'catalog/home.html', context)
+
+def goods(request):
+    return render(request, 'catalog/goods.html')
